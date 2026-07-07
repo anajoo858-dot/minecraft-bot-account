8026700385d97e25acef3d64510d02e0707d622a
anajoo858-dot <anajoo858@gmail.com>
2026-07-08 02:16:24 +0300

    Refactor to use Selenium remote for browser creation
    Updated browser creation to use Selenium remote server and added SELENIUM_REMOTE_URL environment variable.

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 # ============ TOKEN ============
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -18,18 +16,21 @@ if not BOT_TOKEN:
 bot = telebot.TeleBot(BOT_TOKEN)
 user_sessions = {}
 
-# ============ BROWSER ============
+# ============ SELENIUM REMOTE ============
+SELENIUM_URL = os.environ.get("SELENIUM_REMOTE_URL", "http://standalone-chrome.railway.internal:4444/wd/hub")
+
 def create_browser():
     options = ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--remote-debugging-port=9222")
     
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Remote(
+        command_executor=SELENIUM_URL,
+        options=options
+    )
     return driver
 
 def human_typing(element, text):
